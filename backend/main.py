354e22637dd510ee3852be548d9b6e0c3aa039a0
Jack Casey <jackdcasey@gmail.com>
2020-04-13 22:04:14 -0700

    Update main with example code

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,22 @@
 from classes.City import City
 from classes.PhotoSource import PhotoSource
+from classes.ImageProcessing import getAverageColor, getImageArrayFromFile, getImageArrayFromUrl
 
 from typing import List
 
 import jsonpickle
 
+import os
+
 def main():
     cities = loadConfig("cities.json")
 
-    # TODO :)
+    results = {}
+
+    for city in cities:
+        results[city.Name] = [getAverageColor(getImageArrayFromUrl(ps.Url, ps.Kind)) for ps in city.PhotoSourceList]
+
+    print(results)
 
 def loadConfig(filePath) -> List[City]:
     with open(filePath, 'r') as f:
