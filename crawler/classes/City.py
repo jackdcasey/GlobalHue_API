87548dac3b6913d177e3218fa2b845666b00b2d3
Jack Casey <jackdcasey@gmail.com>
2020-04-18 19:06:10 -0700

    Move crawler to GlobalHue_Crawler repo

--- crawler/classes/City.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-from classes.PhotoSource import PhotoSource
-
-class City:
-
-    Name: str
-    PhotoSourceList: List[PhotoSource]
--- crawler/classes/PhotoSource.py
+++
@@ -1,8 +0,0 @@
-class PhotoSource:
-
-    Url: str
-    Kind: str
-    CropTop: int
-    CropBottom: int
-    CropLeft: int
-    CropRight: int
